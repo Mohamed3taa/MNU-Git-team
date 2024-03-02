621f2388cdd402ff8e329cfccd3d0cc47ee4b154
mohamed <mohamed@gmail.com>
2024-03-02 20:22:57 +0200

    master creat

---
+++ __main__ == __name.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 28 13:38:08 2024
+
+@author: pc
+"""
+
+print("hallo")
+if __name__=="__main__":
+    print("mohamed")
+    
+#بنستخدم if __name__=="__main__" عشان لما حد يجي يعمل import لل ال موديول ميعرفش يوصلل لل المعلوماتت ال جوا ال main
+#اي اسكريبت احنا فيه بتبقي __name__ == __main__
